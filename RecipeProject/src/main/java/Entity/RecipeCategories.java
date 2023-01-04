becd5068c8d66a0d38211df821da46a7e67c6df8
BasakAHLAT <basakahlat@ogrenci.beykoz.edu.tr>
2023-01-05 01:09:41 +0300

    Create RecipeCategories.java

---
+++ RecipeProject/src/main/java/Entity/RecipeCategories.java
@@ -0,0 +1,45 @@
+package Entity;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "recipe_categories")
+public class RecipeCategories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
