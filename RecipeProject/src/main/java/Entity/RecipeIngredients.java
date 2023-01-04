d5f9867c21f0480982d2ce98fa0c2585f712fc0c
BasakAHLAT <basakahlat@ogrenci.beykoz.edu.tr>
2023-01-05 01:09:38 +0300

    Create RecipeIngredients.java

---
+++ RecipeProject/src/main/java/Entity/RecipeIngredients.java
@@ -0,0 +1,63 @@
+package Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "recipe_ingredients")
+public class RecipeIngredients {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "recipe_id")
+    private Recipe recipe;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id")
+    private Ingredient ingredient;
+
+    private int quantity;
+    private String unit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
