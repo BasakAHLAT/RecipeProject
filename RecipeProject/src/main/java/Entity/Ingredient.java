6bdaee5fb6936cb8bccb8d9864df01c9106d5dc9
BasakAHLAT <basakahlat@ogrenci.beykoz.edu.tr>
2023-01-05 01:09:54 +0300

    Create Ingredient.java

---
+++ RecipeProject/src/main/java/Entity/Ingredient.java
@@ -0,0 +1,35 @@
+package Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
