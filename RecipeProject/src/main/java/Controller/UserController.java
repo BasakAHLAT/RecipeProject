59e23882a94c6b750e4107557a36cbae6f4b02ae
BasakAHLAT <basakahlat@ogrenci.beykoz.edu.tr>
2023-01-05 01:09:49 +0300

    Create UserController.java

---
+++ RecipeProject/src/main/java/Controller/UserController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import Service.*;
+import Entity.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public @ResponseBody List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public @ResponseBody User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping("/users")
+    public @ResponseBody User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/users/{id}")
+    public @ResponseBody User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public @ResponseBody void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+}
